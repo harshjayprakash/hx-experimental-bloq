8b3f73a3ff1cecbdb5049bb9c2bc3984a43c0d12
Harsh Jayprakash <harshjayprakash@outlook.com>
2025-01-07 13:02:52 +0000

    Add winmain

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#define STRICT 1
+#include <Windows.h>
+
+int WINAPI wWinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR lpCmdLine,
+    _In_ int nCmdShow)
+{
+    return MessageBoxW(NULL, L"Test", L"Test Title", MB_OK);
+}
